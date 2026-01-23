c4f5a943eeba7b304bca182e48ba0d39ff272f98
Iesha567 <IESHA.LONG24@bathspa.ac.uk>
2026-01-23 04:22:26 +0000

    Update sketch.js

--- experiment-3-text-reveal/v2-fade/sketch.js
+++ experiment-3-text-reveal/v2-fade/sketch.js
@@ -15,3 +15,11 @@ function draw() {
   if (frameCount % 4 === 0 && index < poem.length) {
     index++;
   }
+
+  // fade text in as it types
+  let alpha = map(index, 0, poem.length, 30, 255);
+  fill(20, alpha);
+  noStroke();
+  textAlign(LEFT, TOP);
+  text(poem.substring(0, index), 20, 20);
+}
