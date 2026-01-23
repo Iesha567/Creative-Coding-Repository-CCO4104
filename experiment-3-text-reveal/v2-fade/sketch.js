e71738dfad69a2a5f4823e1831377050535df41c
Iesha567 <IESHA.LONG24@bathspa.ac.uk>
2026-01-23 04:21:53 +0000

    Create sketch.js

---
+++ experiment-3-text-reveal/v2-fade/sketch.js
@@ -0,0 +1,17 @@
+let poem = "I learn by doing.\nOne line at a time.\nOne small version at a time.";
+let index = 0;
+
+function setup() {
+  createCanvas(600, 300);
+  textFont("monospace");
+  textSize(22);
+}
+
+function draw() {
+  // gentle shifting background (simple animation)
+  let bg = 235 + 15 * sin(frameCount * 0.03);
+  background(bg);
+
+  if (frameCount % 4 === 0 && index < poem.length) {
+    index++;
+  }
